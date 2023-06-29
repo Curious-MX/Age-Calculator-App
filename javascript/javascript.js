50b04f0b9a3d35ae06fa310a203da599d8013a4d
Maxrichard2007 <max536854@gmail.com>
2023-06-29 09:35:30 +0430

    Age calculator App

---
+++ javascript/javascript.js
@@ -0,0 +1,35 @@
+//  input
+const dayInp = document.getElementById("day");
+const monthInp = document.getElementById("month");
+const yearIn = document.getElementById("year");
+
+// outputs
+const dayOtp = document.getElementById("DD");
+const monthOtp = document.getElementById("MM");
+const yearOtp = document.getElementById("YY");
+
+// form element
+const form = document.querySelector("form");
+
+// adding the submit eventlistenter to form
+form.addEventListener("sjubmit", handleSubmit);
+
+const date = new Date();
+let day = date.getDate();
+let month = 1+ date.getMonth();
+let year = date.getFullYear();
+
+const months = [31, 28, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function validate() {
+    const inputs = document.querySelectorAll("input");
+    let validate = true;
+    inputs.forEach((i) => {
+        const parent = i.parentElement;
+        if (!i.value) {
+            i.style.borderColor = "red";
+            parent.querySelector("small").innerHTML = "this field is required.";
+            validate = false;
+        } else if (mo)
+    })
+}
