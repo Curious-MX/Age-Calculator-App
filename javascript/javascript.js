cd0137d6ec750e7b022b59fd32623a991c60275b
Maxrichard2007 <max536854@gmail.com>
2023-07-01 02:27:53 +0430

    Age calculator App

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -16,20 +16,29 @@ form.addEventListener("sjubmit", handleSubmit);
 
 const date = new Date();
 let day = date.getDate();
-let month = 1+ date.getMonth();
+let month = 1 + date.getMonth();
 let year = date.getFullYear();
 
 const months = [31, 28, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function validate() {
-    const inputs = document.querySelectorAll("input");
-    let validate = true;
-    inputs.forEach((i) => {
-        const parent = i.parentElement;
-        if (!i.value) {
-            i.style.borderColor = "red";
-            parent.querySelector("small").innerHTML = "this field is required.";
-            validate = false;
-        } else if (mo)
-    })
+  const inputs = document.querySelectorAll("input");
+  let validate = true;
+  inputs.forEach((i) => {
+    const parent = i.parentElement;
+    if (!i.value) {
+      i.style.borderColor = "red";
+      parent.querySelector("small").innerHTML = "this field is required.";
+      validate = false;
+    } else if (monthInp.value > 12) {
+      monthInp.style.borderColor = "red";
+      dayInp.parentElement.querySelector("small").innerHTML =
+        "must be a valid day.";
+      validator = false;
+    } else {
+      i.style.borderColor = "black";
+      parent.querySelector("small").innerHTML = "this field is requried.";
+      validator = false;
+    } 
+  });
 }
